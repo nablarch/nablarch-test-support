7a3d4df5bdb9a0dc3c497018e9d639bae89eabbd
heiwa <username.h2s@gmail.com>
2017-03-28 17:09:16 +0900

    test時に使用するスキーマの作成を追加 (#7)

--- src/main/java/nablarch/test/support/db/datasource/H2DbInitializer.java
+++ src/main/java/nablarch/test/support/db/datasource/H2DbInitializer.java
@@ -12,5 +12,6 @@ public class H2DbInitializer implements DbInitializer {
     public void initialize(final Connection connection) throws Exception {
         final Statement statement = connection.createStatement();
         statement.execute("create schema ssd_master");
+        statement.execute("create schema test_schema");
     }
 }
